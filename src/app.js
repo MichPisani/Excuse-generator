7c2f1e6ab1a2643512f2deed091501ee7af600e1
MichPisani <michelinapisani@gmail.com>
2024-11-11 12:09:46 +0000

    final

--- src/app.js
+++ src/app.js
@@ -9,16 +9,16 @@ window.onload = function() {
   function excuseGenerator() {
     /* Primero declaramos las variables (array) */
 
-    let who = ["my dog", "my partner", "my little brother"];
-    let verb = ["ate", "threw", "broke"];
+    let who = ["my dog", "my partner", "my little brother", "my uncle"];
+    let verb = ["threw", "broke", "shattered"];
     let object = ["my homework", "my computer", "my notebook", "my pendrive"];
-    let when = ["this morning", "yesterday night"];
+    let when = ["this morning", "yesterday night", "last weekend"];
 
     /* Elegir una opción de cada array */
-    let whoIndex = Math.floor(Math.random() * 3);
-    let verbIndex = Math.floor(Math.random() * 3);
-    let objectIndex = Math.floor(Math.random() * 4);
-    let whenIndex = Math.floor(Math.random() * 2);
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let verbIndex = Math.floor(Math.random() * verb.length);
+    let objectIndex = Math.floor(Math.random() * object.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
 
     let finalExcuse =
       "I'm so sorry, " +
